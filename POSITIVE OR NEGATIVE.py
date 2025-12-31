c496c387c75563057693e71b9bef320e19b12c0e
Loki's HuB <linduri@student.gitam.edu>
2025-12-31 10:46:44 +0530

    Create POSITIVE OR NEGATIVE.py

---
+++ POSITIVE OR NEGATIVE.py
@@ -0,0 +1,12 @@
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print(f"{num} is Positive")
+elif num < 0:
+    print(f"{num} is Negative")
+else:
+    print(f"{num} is Zero")
+
+OUTPUT:
+Enter a number: 8626
+8626.0 is Positive
