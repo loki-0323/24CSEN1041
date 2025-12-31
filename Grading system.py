398dea658da24c227fee73015687b4348d4aa2b3
Loki's HuB <linduri@student.gitam.edu>
2025-12-31 10:52:02 +0530

    Create Grading system.py

---
+++ Grading system.py
@@ -0,0 +1,19 @@
+marks = float(input("Enter your marks (0-100): "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+elif marks >= 60:
+    grade = "D"
+elif marks >= 50:
+    grade = "E"
+else:
+    grade = "F"
+
+print(f"Your grade is {grade}")
+
+OUTPEnter your marks (0-100): 94
+Your grade is A
