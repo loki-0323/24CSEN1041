08cb2561e71fd09ee07ce246b432d38220777aed
Loki's HuB <linduri@student.gitam.edu>
2025-12-31 10:56:12 +0530

    Create eligible for voting or not.py

---
+++ eligible for voting or not.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+citizen = input("Are you an Indian citizen? (yes/no): ").lower()
+
+if age >= 18 and citizen == "yes":
+    print("You are eligible to vote in India.")
+else:
+    print("You are NOT eligible to vote in India.")
+
+output:
+Enter your age: 20
+Are you an Indian citizen? (yes/no): yes
+You are eligible to vote in India.
