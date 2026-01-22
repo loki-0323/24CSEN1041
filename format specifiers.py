bec04936aeb5db77811eee87af79b3f6026fa28a
Loki's HuB <linduri@student.gitam.edu>
2026-01-22 14:27:23 +0530

    Create format specifiers.py

---
+++ format specifiers.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a number: "))
+
+print(f"Decimal representation: {a}")
+print(f"Decimal representation: {bin(a)}")
+print(f"Octal representation: {oct(a)}")
+print(f"Hexadecimal representation: {hex(a)}")
+
+Output:
+Enter a number: 24
+Decimal representation: 24
+Decimal representation: 0b11000
+Octal representation: 0o30
+Hexadecimal representation: 0x18
