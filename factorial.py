81654f7bc28bcd7601af46542eda75c9d4a33f6d
Loki's HuB <linduri@student.gitam.edu>
2026-02-02 14:57:47 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):    
+      result *= i
+    return result
+
+print(f"Factorial of {num} is {factorial(num)}")
+
+Output:
+Enter a number: 5
+Factorial of 5 is 120
