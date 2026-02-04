a80e0dcb05a4d4afd190b904ff6ea426315846ee
Loki's HuB <linduri@student.gitam.edu>
2026-02-04 11:20:43 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+text = input("Enter a string: ")
+num = text.upper()
+if num == num [::-1]:
+    print("It is a palindrome!")
+else:
+    print("Not a palindrome.")
+    
+print("length of string: ", len(text))
+
+Output:
+Enter a string: aB12321bA
+It is a palindrome!
+length of string:  9
